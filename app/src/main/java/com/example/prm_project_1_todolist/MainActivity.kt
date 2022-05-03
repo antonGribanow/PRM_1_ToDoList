c3bdf7e897b1c195ab322713e9e583de921c5dc3
Anton Gribanow <anton.gribanow@icloud.com>
2022-05-03 16:16:04 +0200

    first commit

---
+++ app/src/main/java/com/example/prm_project_1_todolist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.prm_project_1_todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
